0ca6009d54b85ab255f97dc6bc3eb11599395cad
Naelya <zhandaralievanaelya@gmail.com>
2022-03-31 12:34:50 +0600

    names

---
+++ btn.js
@@ -0,0 +1,13 @@
+document.getElementById('input').addEventListener("click", clickIn)
+
+function clickIn(event) {
+    event.preventDefault();
+    var elemant = document.getElementById('input')
+var proger  = ["Ann","Harry","Anara"]
+   
+   var proger = elemant.proger.value
+    
+   
+console.log(proger);
+   
+}
